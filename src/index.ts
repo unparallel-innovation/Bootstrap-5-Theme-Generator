9cae9d4d2a8d33b606eb307a66fe7225de606599
António Gonçalves <antonio.goncalves@unparallel.pt>
2024-02-02 12:06:43 +0000

    Added BootstrapTheme to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import fs from "node:fs";
-import Bootstrap5Generator,{Cache,Bootstrap5GeneratorConstructor,BootstrapVariables} from "./Bootstrap5Generator"
+import Bootstrap5Generator,{Cache,Bootstrap5GeneratorConstructor,BootstrapVariables,BootstrapTheme} from "./Bootstrap5Generator"
 
-export type {Cache,Bootstrap5GeneratorConstructor,BootstrapVariables}
+export type {Cache,Bootstrap5GeneratorConstructor,BootstrapVariables,BootstrapTheme}
 export {Bootstrap5Generator}
