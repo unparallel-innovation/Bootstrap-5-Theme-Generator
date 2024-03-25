58079709e2ea0592338a81f2aa13cb5f4cc6b9f3
António Gonçalves <antonio.goncalves@unparallel.pt>
2024-03-25 18:11:46 +0000

    Added support to custom SCSS

--- dev/index.ts
+++ dev/index.ts
@@ -4,22 +4,22 @@ const Bootstrap5Generator = require("../").Bootstrap5Generator
 
 
 
-const  storage:any = {}
+const storage: any = {}
 
 const cache = {
-	setCachedCSS:(id:string,css:string)=>{
+	setCachedCSS: (id: string, css: string) => {
 		storage[id] = css
 	},
-	getCachedCSS:(id:string)=>(storage[id])
+	getCachedCSS: (id: string) => (storage[id])
 }
 
 const variables = {
-	"$primary": "#004968",
+	"$primary": "#FFaaCC",
 	"$secondary": "#ccdbe1",
 	"$warning": "#ee8140",
 	"$danger": "#CC3105",
 	"$success": "#B6E186",
-	"$body-bg": "#001e33",
+	"$body-bg": "#ffff00",
 	"$info": "#99b6c3",
 	"$badge-pill-border-radius": "20rem",
 	"$border-radius": "1.5rem",
@@ -29,22 +29,31 @@ const variables = {
 }
 
 const theme = {
-	variables
+	variables,
+	scss: {
+		after: `
+			:root {
+				--light-color:#{$light};
+				--test-var:#{$primary};
+			}
+		
+		`
+	}
 }
 
-const instance = new Bootstrap5Generator({cache})
+const instance = new Bootstrap5Generator({ cache, ignoreMinify: false })
 
-async function main(){
-	try{
+async function main() {
+	try {
 		console.log("Generating")
 		console.time()
 		const css = await instance.getCSS(theme)
-		fs.writeFileSync(__dirname + '/test.css',css)
+		fs.writeFileSync(__dirname + '/test.css', css)
 		console.timeEnd()
 		console.log("Generating")
 		console.time()
 		const css2 = await instance.getCSS(theme)
-		fs.writeFileSync(__dirname + '/test2.css',css2)
+		fs.writeFileSync(__dirname + '/test2.css', css2)
 		console.timeEnd()
 	}
 	catch (e) {
--- src/Bootstrap5Generator/index.ts
+++ src/Bootstrap5Generator/index.ts
@@ -2,70 +2,92 @@ import fs from "node:fs";
 import * as sass from 'sass'
 import hash from 'object-hash'
 
+export type BoostrapThemeSCSS = string | {
+	before?: string,
+	after?: string
+}
 
 export interface BootstrapTheme {
-	variables:BootstrapVariables,
-	[key:string]:any
+	variables?: BootstrapVariables,
+	scss?: BoostrapThemeSCSS,
+	[key: string]: any
 }
 
 export interface BootstrapVariables {
-	[key:string]:string
+	[key: string]: string
 }
 
 export interface Cache {
-	getCachedCSS:(id:string,theme:BootstrapTheme)=>(string | undefined | null | Promise<string | undefined | null>),
-	setCachedCSS:(id:string,css:string,theme:BootstrapTheme)=>(Promise<void> | void)
+	getCachedCSS: (id: string, theme: BootstrapTheme) => (string | undefined | null | Promise<string | undefined | null>),
+	setCachedCSS: (id: string, css: string, theme: BootstrapTheme) => (Promise<void> | void)
 
 }
 
 
 export interface Bootstrap5GeneratorConstructor {
-	cache?:Cache,
-	ignoreMinify?:boolean
+	cache?: Cache,
+	ignoreMinify?: boolean
 }
 export default class Bootstrap5Generator {
 
-	private cache: Cache | undefined
-	private ignoreMinify: boolean;
-	
-	constructor(props?:Bootstrap5GeneratorConstructor) {
-		this.cache = props?.cache
-		this.ignoreMinify = !!props?.ignoreMinify
+	#cache: Cache | undefined
+	#ignoreMinify: boolean;
+
+	constructor(props?: Bootstrap5GeneratorConstructor) {
+		this.#cache = props?.cache
+		this.#ignoreMinify = !!props?.ignoreMinify
 
 	}
 
 
-	generateCSS(variables:BootstrapVariables = {}):string{
-		const css = Object.keys(variables).reduce((acc,val)=>(
+	generateCSS(theme: BootstrapTheme): string {
+		const variables = theme.variables || {}
+		let scssBefore: string = ""
+		let scssAfter: string = ""
+		if (typeof theme.scss === "string") {
+			scssAfter = theme.scss
+		} else {
+			scssBefore = theme.scss?.before || ""
+			scssAfter = theme.scss?.after || ""
+		}
+		const variableSCSS = Object.keys(variables).reduce((acc, val) => (
 			`${acc}${val}:${variables[val]};`
-		),"")
+		), "")
 		const result = sass.compileString(`
-				${css}
+				${variableSCSS}
+				${scssBefore} 
 				@import "bootstrap";
-			`,{
-			style:this.ignoreMinify?"expanded":"compressed",
-			loadPaths:[`${__dirname}/bootstrap/scss`]
+				${scssAfter}
+			`, {
+			style: this.#ignoreMinify ? "expanded" : "compressed",
+			loadPaths: [`${__dirname}/bootstrap/scss`]
 		})
+
 		return result.css
 
 	}
 
-	async cacheCSS(theme:BootstrapTheme):Promise<string>{
-		if(!theme?.variables) return ""
-		const variables = theme.variables
-		const generatedCSS =  this.generateCSS(variables)
-		const id = hash(variables)
-		await this.cache?.setCachedCSS(id,generatedCSS,theme)
+	async cacheCSS(theme: BootstrapTheme): Promise<string> {
+		if (!theme) return ""
+		const generatedCSS = this.generateCSS(theme)
+		const id = this.#getHashFromTheme(theme)
+		await this.#cache?.setCachedCSS(id, generatedCSS, theme)
 		return generatedCSS
 	}
 
-	async getCSS(theme:BootstrapTheme):Promise<string>{
-		const variables = theme.variables
-		const id = hash(variables)
-		const cachedCSS = await this.cache?.getCachedCSS(id,theme)
-		if(cachedCSS) return cachedCSS
-		const generatedCSS =  this.generateCSS(variables)
-		this.cache?.setCachedCSS(id,generatedCSS,theme)
+	#getHashFromTheme(theme: BootstrapTheme): string {
+		const { css, variables } = theme
+		const obj = { variables, css }
+		return hash(obj)
+	}
+
+
+	async getCSS(theme: BootstrapTheme): Promise<string> {
+		const id = this.#getHashFromTheme(theme)
+		const cachedCSS = await this.#cache?.getCachedCSS(id, theme)
+		if (cachedCSS) return cachedCSS
+		const generatedCSS = this.generateCSS(theme)
+		this.#cache?.setCachedCSS(id, generatedCSS, theme)
 		return generatedCSS
 
 	}
